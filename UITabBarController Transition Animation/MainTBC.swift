c3387e0b3587b8ff9f7e838683acd217acefe64d
NP2 <sahandraeisi1994@gmail.com>
2019-05-22 11:58:47 +0430

    Add origin view to transition

--- UITabBarController Transition Animation/MainTBC.swift
+++ UITabBarController Transition Animation/MainTBC.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MainTBC: UITabBarController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,31 +23,50 @@ class MainTBC: UITabBarController {
 extension MainTBC: UITabBarControllerDelegate {
 
     func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return TabBarAnimatedTransitioning()
+        return SHNDTabBarTransitioning()
     }
 }
 
-final class TabBarAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+final class SHNDTabBarTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
     
     fileprivate enum SHNDTabBarAnimation {
         case scaleX
         case scaleY
         case scaleXY
+        case rightToLeft
+        case leftToRight
+        case upToDown
+        case downToUp
+        case fade
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let origin = transitionContext.view(forKey: .from) else { return }
         guard let destination = transitionContext.view(forKey: UITransitionContextViewKey.to) else { return }
         
-        destination.alpha = 0.0
-        destination.transform = CGAffineTransform(translationX: 0, y: -destination.frame.size.height)
-        destination.transform = CGAffineTransform(scaleX: 1.4, y: 1)
+        origin.alpha = 1
+        origin.transform = .identity
         
+        destination.alpha = 0.0
+        destination.transform = CGAffineTransform(rotationAngle: .pi / 4)
+        
+        transitionContext.containerView.addSubview(origin)
         transitionContext.containerView.addSubview(destination)
         
+        
         UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, options: .curveEaseOut, animations: {
+            
+            origin.alpha = 0
+            origin.transform = CGAffineTransform(translationX: +100, y: 0)
+                //CGAffineTransform(scaleX: 1.5, y: 1.5)
+            
             destination.alpha = 1.0
             destination.transform = .identity
+            
         }) { (success) in
+            origin.transform = .identity
+            origin.alpha = 1
             transitionContext.completeTransition(success)
         }
     }
